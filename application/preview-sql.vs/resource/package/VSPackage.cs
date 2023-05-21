46de33966b442090af0e67097180453f3ecd69e8
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-05-21 19:28:14 +0300

    - Used NuGet packages are updated

--- application/preview-sql.vs/resource/package/VSPackage.cs
+++ application/preview-sql.vs/resource/package/VSPackage.cs
@@ -1,11 +1,6 @@
-﻿
-using EnvDTE;
-using EnvDTE80;
-using Microsoft.VisualStudio;
+﻿using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Task = System.Threading.Tasks.Task;
@@ -33,43 +28,44 @@ namespace resource.package
         {
             {
                 extension.AnyPreview.Connect();
+                //extension.AnyPreview.Connect(CONSTANT.APPLICATION, CONSTANT.NAME);
                 extension.AnyPreview.Register(".SQL", new preview.VSPreview());
             }
             {
                 await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             }
-            try
-            {
-                if (string.IsNullOrEmpty(atom.Trace.GetFailState(CONSTANT.APPLICATION)) == false)
-                {
-                    var a_Context = Package.GetGlobalService(typeof(SDTE)) as DTE2;
-                    if (a_Context != null)
-                    {
-                        var a_Context1 = (OutputWindowPane)null;
-                        for (var i = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Count; i >= 1; i--)
-                        {
-                            if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == CONSTANT.HOST)
-                            {
-                                a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i);
-                                break;
-                            }
-                        }
-                        if (a_Context1 == null)
-                        {
-                            a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add(CONSTANT.HOST);
-                        }
-                        if (a_Context1 != null)
-                        {
-                            a_Context1.OutputString("\r\n" + CONSTANT.NAME + " extension doesn't work without MetaOutput.\r\n    Please install it --> https://www.metaoutput.net/download\r\n");
-                            a_Context1.Activate();
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.ToString());
-            }
+            //try
+            //{
+            //    if (string.IsNullOrEmpty(atom.Trace.GetFailState(CONSTANT.APPLICATION)) == false)
+            //    {
+            //        var a_Context = Package.GetGlobalService(typeof(SDTE)) as DTE2;
+            //        if (a_Context != null)
+            //        {
+            //            var a_Context1 = (OutputWindowPane)null;
+            //            for (var i = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Count; i >= 1; i--)
+            //            {
+            //                if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == CONSTANT.HOST)
+            //                {
+            //                    a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i);
+            //                    break;
+            //                }
+            //            }
+            //            if (a_Context1 == null)
+            //            {
+            //                a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add(CONSTANT.HOST);
+            //            }
+            //            if (a_Context1 != null)
+            //            {
+            //                a_Context1.OutputString("\r\n" + CONSTANT.NAME + " extension doesn't work without MetaOutput.\r\n    Please install it --> https://www.metaoutput.net/download\r\n");
+            //                a_Context1.Activate();
+            //            }
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.ToString());
+            //}
         }
 
         protected override int QueryClose(out bool canClose)
